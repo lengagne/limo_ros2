e18f0207984758a6902b74f1afcdfa2eaf979507
= <lengagne@gmail.com>
2025-11-19 15:43:24 +0100

    at least simulation works

--- limo_arena/limo_arena/referee.py
+++ limo_arena/limo_arena/referee.py
@@ -1,10 +1,12 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Pose
+from std_msgs.msg import Int32
 from gazebo_msgs.msg import ModelStates
 from gazebo_msgs.srv import SetEntityState
 from gazebo_msgs.msg import EntityState
 from gazebo_msgs.srv import SpawnEntity
+
 
 class BallonSpawner(Node):
     def __init__(self):
@@ -23,9 +25,16 @@ class BallonSpawner(Node):
             '/gazebo/model_states',
             self.model_states_callback,
             10)
+        
+        self.subscription_reset = self.create_subscription( Int32, '/reset_score', self.reset_score,10)
+        
         self.client = self.create_client(SetEntityState, '/gazebo/set_entity_state')
         self.ballon_name = "ballon"
-        self.goal_line = 1.5  # Coordonnée x de la ligne de but      
+        self.goal_line = 1.5  # Coordonnée x de la ligne de but
+        
+        
+        self.score1 = 0
+        self.score2 = 0
         
     def model_states_callback(self, msg):
         try:
@@ -33,20 +42,31 @@ class BallonSpawner(Node):
                 index = msg.name.index(self.ballon_names[i])
                 x = msg.pose[index].position.x
                 if x > self.goal_line:
+                    self.score1 += 1
                     self.reset_ballon(self.ballon_names[i])
+                    
                 if x < - self.goal_line:
+                    self.score2 += 1
                     self.reset_ballon(self.ballon_names[i])                    
+                    
         except ValueError:
             pass        
         
     def reset_ballon(self,name):
         self.get_logger().info('Reset ball {name}')
+        self.get_logger().info('Score : %d : %d' %(self.score1, self.score2))
         request = SetEntityState.Request()
         request.state.name = name
         request.state.pose.position.x = 0.0
         request.state.pose.position.y = 0.0
         request.state.pose.position.z = 0.5
         self.client.call_async(request)
+
+    def reset_score(self,msg):
+        self.get_logger().info('Reset Score')
+        self.score1 = 0
+        self.score2 = 0
+        self.get_logger().info('Score : %d : %d' %(self.score1, self.score2))
 
     def spawn_ballon(self, name,y):
         request = SpawnEntity.Request()
--- limo_arena/setup.py
+++ limo_arena/setup.py
@@ -1,6 +1,6 @@
 import os
 from glob import glob
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'limo_arena'
 
@@ -9,23 +9,21 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join( 'share', package_name, 'launch'), glob (os.path.join('launch', '*launch.[pxy][yma]*'))),
         ('share/' + package_name + '/worlds', ['worlds/arena.world']),
-        
+        (os.path.join( 'share', package_name, 'launch'), glob (os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Sebastie Lengagne',
+    maintainer='Sebastien Lengagne',
     maintainer_email='lengagne@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Package pour la gestion de l\'arène LIMO',
+    license='Apache 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'referee=limo_arena.referee:main'
+            'referee = limo_arena.referee:main',
         ],
-    }
+    },
 )
