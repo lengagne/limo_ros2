19c1da4229ce9259aa663c4cc7a1e5e957c23f34
Sébastien Lengagne <lengagne@gmail.com>
2025-11-25 13:18:39 +0100

    can simulated 2 robots

---
+++ limo_arena/launch/limo_arena_2robots.launch.py
@@ -0,0 +1,79 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Nom du package
+    package_name = 'limo_arena'
+    package_env_name = 'limo_simulation'
+
+    # Crée la description de launch
+    ld = LaunchDescription()
+
+    # Chemin vers le fichier gazebo_simulation_common.launch.py
+    simulation_launch_path = os.path.join(
+        get_package_share_directory(package_env_name),
+        'launch',
+        'gazebo_simulation_common.launch.py'
+    )
+
+    # Chemin vers le fichier gazebo_simulation_common.launch.py
+    spawn_robot_launch_path = os.path.join(
+        get_package_share_directory(package_env_name),
+        'launch',
+        'gazebo_spawn_common.launch.py'
+    )
+
+
+    simulation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(simulation_launch_path),
+        launch_arguments={
+            'environnement_path': 'worlds/arena.world',  # Chemin relatif
+            'env_pkg': package_name,
+        }.items()
+    )
+
+    # Inclut le fichier gazebo_simulation_common.launch.py et modifie ses paramètres
+    spawn_robot1_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(spawn_robot_launch_path),
+        launch_arguments={
+            'spawn_x_val': '1.5',
+            'spawn_y_val': '0.0',
+            'spawn_z_val': '0.0',
+            'spawn_yaw_val': '3.14',
+            'robot_name' : 'limo1',
+        }.items()
+    )
+
+    # Inclut le fichier gazebo_simulation_common.launch.py et modifie ses paramètres
+    spawn_robot2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(spawn_robot_launch_path),
+        launch_arguments={
+            'spawn_x_val': '-1.5',
+            'spawn_y_val': '0.0',
+            'spawn_z_val': '0.0',
+            'spawn_yaw_val': '0',
+            'robot_name' : 'limo2',
+        }.items()
+    )
+
+    ## Add referee
+    #referee_node = Node(
+        #package=package_name,  # Remplace par le nom du package contenant le nœud referee
+        #executable='referee',  # Remplace par le nom de l'exécutable du nœud referee
+        #name='referee',
+        #output='screen',  # Affiche la sortie du nœud dans le terminal
+    #)
+
+    ld.add_action(simulation_launch)
+    ld.add_action(spawn_robot1_launch)
+    ld.add_action(spawn_robot2_launch)
+    #ld.add_action(referee_node)
+
+    return ld
--- limo_simulation/launch/gazebo_spawn_common.launch.py
+++ limo_simulation/launch/gazebo_spawn_common.launch.py
@@ -1,6 +1,6 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, LogInfo
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, LaunchConfiguration, PythonExpression, PathJoinSubstitution
@@ -44,27 +44,30 @@ def generate_launch_description():
         )
     )
 
-    # Constants for paths to different files and folders
-    package_name = 'limo_simulation'
-    robot_name_in_model = 'limo_simulation'
-    urdf_file_path = 'urdf/limo_four_diff.xacro'
-    pkg_share = FindPackageShare(package=package_name)
-    default_urdf_model_path = os.path.join(pkg_share.find(package_name), urdf_file_path)
-
-    # Pose where we want to spawn the robot
-    spawn_x_val = LaunchConfiguration('spawn_x_val')   
-    spawn_y_val = LaunchConfiguration('spawn_y_val')   
-    spawn_z_val = LaunchConfiguration('spawn_z_val')   
-    spawn_yaw_val = LaunchConfiguration('spawn_yaw_val')   
-
-
     ld.add_action(
         DeclareLaunchArgument(
-            name='namespace',
+            name='robot_name',
             default_value='',
             description='Namespace de niveau supérieur'
         )
     )
+
+    spawn_x_val = LaunchConfiguration('spawn_x_val')
+    spawn_y_val = LaunchConfiguration('spawn_y_val')
+    spawn_z_val = LaunchConfiguration('spawn_z_val')
+    spawn_yaw_val = LaunchConfiguration('spawn_yaw_val')
+    robot_name = LaunchConfiguration('robot_name')
+    urdf_model = LaunchConfiguration('urdf_model')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+
+    # Constants for paths to different files and folders
+    package_name = 'limo_simulation'
+    robot_name_in_model = robot_name
+    urdf_file_path = 'urdf/limo_four_diff.xacro'
+    pkg_share = FindPackageShare(package=package_name)
+    default_urdf_model_path = os.path.join(pkg_share.find(package_name), urdf_file_path)
+
 
     ld.add_action(
         DeclareLaunchArgument(
@@ -74,42 +77,36 @@ def generate_launch_description():
         )
     )
 
-    ld.add_action(
-        DeclareLaunchArgument(
-            name='use_robot_state_pub',
-            default_value='True',
-            description='Démarrer le publisher d\'état du robot si vrai'
-        )
-    )
 
-    ld.add_action(
-        DeclareLaunchArgument(
-            name='use_sim_time',
-            default_value='True',
-            description='Utiliser l\'horloge de simulation (Gazebo) si vrai'
-        )
-    )
 
-    namespace = LaunchConfiguration('namespace')
-    urdf_model = LaunchConfiguration('urdf_model')
-    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
-    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    log_X = LogInfo(msg=['La valeur de spawn_x_val est : ', spawn_x_val])
+    log_namespace = LogInfo(msg=['La valeur de robot_name est : ', robot_name])
+
+    robot_description = Command([
+        'xacro ', urdf_model,
+        ' robot_namespace:=', robot_name,
+    ])
+
 
     # Subscribe to the joint states of the robot, and publish the 3D pose of each link
     start_robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
+        namespace= robot_name,
         parameters=[
-            {'robot_description': Command(['xacro ', urdf_model])},
+            {'robot_description': robot_description},
             {'use_sim_time': use_sim_time}
-        ]
+        ],
+
     )
 
     # Launch the robot
     spawn_entity_cmd = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
+        namespace= robot_name,
         arguments=[
             '-entity', robot_name_in_model,
             '-topic', 'robot_description',
@@ -124,6 +121,8 @@ def generate_launch_description():
     # Add all actions to the launch description
     ld.add_action(start_robot_state_publisher_cmd)
     ld.add_action(spawn_entity_cmd)
+    ld.add_action(log_X)
+    ld.add_action(log_namespace)
 
     return ld
 
